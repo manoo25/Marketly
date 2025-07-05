d80d8287e7f3face320841fe13975e98c313b8d9
manoo25 <mohamedsalamma25@gmail.com>
2025-07-05 17:38:02 +0300

    Bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import { Store } from './Redux/Store'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
     <StrictMode>
-    <App />
-  </StrictMode>
+      <App />
+    </StrictMode>
   </Provider>,
 )
