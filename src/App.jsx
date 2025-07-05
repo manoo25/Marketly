1941c75bd3910e8cbfe6254e3dd656649e5b7c89
RashaSolia <rashasolia@gmail.com>
2025-07-06 01:14:18 +0300

    user modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-
-import { } from "./Redux/Slices/Users";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import { AddCategory, DeleteCategory, GetCategories, UpdateCategory } from "./Redux/Slices/Categories";
 import Sidebar from "./Components/LayoutComponents/Sidebar";
+import AddUserModal from "./Components/modalsComponents/UserModal";
 
 function App() {
-  
+  const [showModal, setShowModal] = useState(false);
+
   return (
-   <Sidebar></Sidebar>
+    <>
+      <Sidebar />
+
+      <div className="text-end p-4">
+        <Button variant="primary" onClick={() => setShowModal(true)}>
+          إضافة مستخدم
+        </Button>
+
+        <AddUserModal show={showModal} handleClose={() => setShowModal(false)} />
+      </div>
+    </>
   );
 }
 
