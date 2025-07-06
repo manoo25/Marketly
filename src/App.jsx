124eae78ba5f71c0ada39bfc58f3e222699123e3
ahmedgaafer1 <midogaafer67@gmail.com>
2025-07-06 07:52:57 +0300

    finish users table and productsRedux

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { AddCategory, DeleteCategory, GetCategories, UpdateCategory } from "./Redux/Slices/Categories";
 import Sidebar from "./Components/LayoutComponents/Sidebar";
 import AllGlobalCopm from "./Pages/AllGlobalCopm";
+import UsersTbl from "./Components/UsersComponents/UsersTbl";
 
 function App() {
  
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { Provider } from 'react-redux'
-import { Store } from './Redux/Store'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { Store } from "./Redux/Store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
     <StrictMode>
-      <App />
+    <App />
     </StrictMode>
-  </Provider>,
-)
-
-
-createRoot(document.getElementById('root')).render(
-  <Provider store={Store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </Provider>,
-)
+  </Provider>
+);
