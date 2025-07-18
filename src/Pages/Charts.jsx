5fac0e4b2fa7a13447b36efb1fa250bbc618ba74
Ahmedyasser7417 <ahmedyassernti2212@gmail.com>
2025-07-18 15:23:22 +0300

    update charts

--- src/Pages/Charts.jsx
+++ src/Pages/Charts.jsx
@@ -1,11 +1,13 @@
+import StatsGrid from "../Components/ChartsComponents/StatsGrid";
 import Loading from "../Components/globalComonents/loading";
 
 function Charts() {
-    return ( <>
-    
-    <p>Charts Page</p>
-  
-    </> );
+    return ( 
+        <div className="space-y-6 pt-3">
+            <StatsGrid />
+        </div>
+      
+     );
 }
 
 export default Charts;
